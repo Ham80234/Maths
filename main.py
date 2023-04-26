0227cbe47be18a980a3b5554c21d87c2da92b00e
Andrew <Ham80234@gmail.com>
2023-04-26 10:01:25 -0500

    code

---
+++ main.py
@@ -0,0 +1,21 @@
+# run pip3 install matplotlib numpy 
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+P=np.linspace(0.7,4,10000)
+m=0.7
+
+X = []
+Y = []
+
+for u in P:
+    X.append(u)
+    m = np.random.random()
+    for n in range(1001):
+      m=(u*m)*(1-m)
+    for l in range(1051):
+      m=(u*m)*(1-m)
+    Y.append(m)
+plt.plot(X, Y, ls='', marker=',')
+plt.show()
